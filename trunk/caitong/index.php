e5ce50cd6fe7712a011dacbc63d9965748831399
luotimhua@gmail.com <luotimhua@gmail.com@f01b8778-c1c8-e007-fbd2-a7ee36920cdb>
2013-04-24 13:21:06 +0000

    上传静态页面-龙上波

---
+++ trunk/caitong/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>彩通</title>
+<link type="text/css" rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+	<div class="wrap">
+		<!-- 顶部 -->
+		<?php
+			require_once("top.php");
+		?>
+		<div class="ct_location">
+			当前位置：&nbsp;<a href="index.php">首页</a>
+		</div>
+		<!-- 中间开始 -->
+		<div class="ct_middle">
+			<?php
+				require_once("typeList.php");
+			?>
+			<div class="ct_middle_right">
+				<div class = "ct_middle_scrollImg">
+					 <script type="text/javascript" src="script/flash_img.js"></script>
+				</div>
+				<div class = "ct_middle_goods_list">
+					<div class = "ct_middle_goods_title">
+						<div style = "float:left;"><img src = "images/location_hot.gif" />&nbsp;产品展示</div>
+						<div style = "float:right;margin-right:10px;">&gt;&gt;更多</div>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src="images/goods.gif" width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif" width="125" height="100"  /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif"  width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif"  width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif"  width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif"  width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif"  width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+					<div class="ct_goods_info">
+						 <ul>
+						    <li><a href = "#"><img src = "images/goods.gif"  width="125" height="100" /></a></li>
+						    <li><font color = "#38B0E3">HTC Touch HD</font></li>
+						    <li>RMB 200</li>
+						    <li class = "add_to_cart"><a href = "#">Add to Cart</a></li>
+						  </ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- 中间结束 -->
+		<div style = "clear:both;"></div>
+		<?php
+			require_once("copy.php");
+		?>
+	</div>
+</body>
+</html>

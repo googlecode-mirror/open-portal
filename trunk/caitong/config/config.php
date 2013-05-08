f08d910461f086d6cca0ff317591e4561a4d0bd7
luotimhua@gmail.com <luotimhua@gmail.com@f01b8778-c1c8-e007-fbd2-a7ee36920cdb>
2013-05-08 15:46:21 +0000

    提交配置文件

---
+++ trunk/caitong/config/config.php
@@ -0,0 +1,7 @@
+<?php
+$config["db"]["dbhost"] = "localhost";
+$config["db"]["dbuser"] = "root";
+$config["db"]["dbpwd"] = "";
+$config["db"]["dbname"] = "test";
+$config["db"]["dbcharset"] = "utf-8";
+$config["db"]["pconnect"] = "0";

f1957eb0ef0fdf03a787b6549117c0736f1fcd1c
luotimhua <luotimhua@gmail.com>
2013-05-08 16:17:19 +0000

    提交应用入口文件

---
+++ caitong/framework/Application.php
@@ -0,0 +1,69 @@
+<?php
+//
+//  Application类
+//
+
+class Application {
+	
+	/**
+	 ** 配置类
+	 ** @var  Array
+	 **/
+	private $config;
+	
+	// 应用名称
+	// @var  String
+	private $appName;
+	
+    // 类加载程序
+	private $loader;
+	
+	public function __construct($appName, $config = array()) {
+		$this->config = $config;
+		
+	}
+	
+	// 返回配置信息
+	// @var  Array
+	public function getConfig() {
+		return $this->config;
+	}
+	
+	// 初始化应用程序
+	// @return  
+	public function init() {
+		$this->initEnv();
+		$this->initDatabase();
+		$this->initLogger();
+		return $this;
+	}
+	
+	public function run() {
+		$module = $_GET["mod"];
+		$action = $_GET["act"];
+		
+		// TODO: can do some filter here
+		
+		$dispatcher = new RequestDispatch($this, $module, $action);
+		$dispatcher.dispatch();
+	}
+	
+	//
+	// private methods
+	//
+	
+	// 初始化环境参数
+	private function initEnv() {
+		$this->loader = new classloader();
+	}
+	
+	// 初始化数据库
+	private function initDatabase() {
+		database::init($config["db"]);
+	}
+	
+	// 初始化日志
+	private function initLogger() {
+		logger::init(array());
+	}
+}
